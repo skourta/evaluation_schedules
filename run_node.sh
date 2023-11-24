2e11fddc65c1a5cd3b38f74c67683a3e83339eb4
Smail KOURTA <sk10691@nyu.edu>
2023-11-24 15:19:11 +0400

    added executing unoptimized version at the same time

--- run_node.sh
+++ run_node.sh
@@ -10,11 +10,16 @@
 #SBATCH --mem=102G
 #SBATCH --reservation=c2
 
-
 # check if the user provided the name of the benchmark folder
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
     exit 1
+fi
+# check if the user provided a 2nd argument (Execute unoptimized or not)
+if [ $# -eq 2 ]; then
+    EXECUTE_UNOPTIMIZED=$2
+else
+    EXECUTE_UNOPTIMIZED=0
 fi
 
 echo "Running on $(hostname)"
@@ -25,11 +30,12 @@ echo "SLURM_JOB_NODELIST=$SLURM_JOB_NODELIST"
 echo "Benchmarks to execute: $1"
 # print the list of files to be executed
 echo "Files to execute: $(ls $1)"
+echo "Execute unoptimized: $EXECUTE_UNOPTIMIZED"
 
 echo
 echo
 echo "--------------------------------------------------"
-echo 
+echo
 echo
 
 TIRAMISU_ROOT=/home/sk10691/tiramisu_new
@@ -43,15 +49,13 @@ LD_LIBRARY_PATH=:$CONDA_ENV/lib:${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64:$
 export LD_LIBRARY_PATH
 
 WRAPPER=/scratch/sk10691/workspace/rl/evaluation_schedules/wrappers
-
+GENERATORS=/scratch/sk10691/workspace/rl/evaluation_schedules/generators
 suffix=".cpp"
 
-BENCHMARKS_PATH=$1
+export MAX_RUNS=10
+export NB_EXEC=10
 
-# if the user provided the name of the benchmark folder as "generators" set suffix to _generator.cpp
-if [ $BENCHMARKS_PATH = "generators" ]; then
-    suffix="_generator.cpp"
-fi
+BENCHMARKS_PATH=$1
 
 cd $BENCHMARKS_PATH
 
@@ -60,7 +64,6 @@ RESULTS_DIR="results"
 
 mkdir $WORKDIR_DIR
 mkdir $RESULTS_DIR
-
 
 for f in ./*.cpp; do
     # get the function name
@@ -71,22 +74,52 @@ for f in ./*.cpp; do
     # copy the files to the WORKDIR_DIR
     cp ./${FUNC_NAME}${suffix} $WORKDIR_DIR
     cp $WRAPPER/${FUNC_NAME}_wrapper* $WORKDIR_DIR
+    if [ $EXECUTE_UNOPTIMIZED -eq 1 ]; then
+        cp $GENERATORS/${FUNC_NAME}_generator.cpp $WORKDIR_DIR/${FUNC_NAME}_unoptimized.cpp
+    fi
 
     # go to the WORKDIR_DIR
     cd $WORKDIR_DIR
 
-    # compile the generator and run it
+    # compile the generator of the schedule and run it
     ${CXX} -I${TIRAMISU_ROOT}/3rdParty/Halide/install/include -I${TIRAMISU_ROOT}/include -I${TIRAMISU_ROOT}/3rdParty/isl/include -Wl,--no-as-needed -ldl -g -fno-rtti -lpthread -fopenmp -std=c++17 -O0 -o ${FUNC_NAME}_generator.cpp.o -c ${FUNC_NAME}.cpp
 
     ${CXX} -Wl,--no-as-needed -ldl -g -fno-rtti -lpthread -fopenmp -std=c++17 -O0 ${FUNC_NAME}_generator.cpp.o -o ./${FUNC_NAME}_generator -L${TIRAMISU_ROOT}/build -L${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64 -L${TIRAMISU_ROOT}/3rdParty/isl/build/lib -Wl,-rpath,${TIRAMISU_ROOT}/build:${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64:${TIRAMISU_ROOT}/3rdParty/isl/build/lib -ltiramisu -ltiramisu_auto_scheduler -lHalide -lisl
 
     ./${FUNC_NAME}_generator
 
-    # compile the wrapper and run it
+    # create shared object from the generated object file
     ${CXX} -shared -o ${FUNC_NAME}.o.so ${FUNC_NAME}.o
+
+    # compile the wrapper
     ${CXX} -std=c++17 -fno-rtti -I${TIRAMISU_ROOT}/include -I${TIRAMISU_ROOT}/3rdParty/Halide/install/include -I${TIRAMISU_ROOT}/3rdParty/isl/include/ -I${TIRAMISU_ROOT}/benchmarks -L${TIRAMISU_ROOT}/build -L${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64/ -L${TIRAMISU_ROOT}/3rdParty/isl/build/lib -o ${FUNC_NAME}_wrapper -ltiramisu -lHalide -ldl -lpthread -fopenmp -lm -Wl,-rpath,${TIRAMISU_ROOT}/build ./${FUNC_NAME}_wrapper.cpp ./${FUNC_NAME}.o.so -ltiramisu -lHalide -ldl -lpthread -fopenmp -lm -lisl
 
+    # Compile the unoptimized version
+    if [ $EXECUTE_UNOPTIMIZED -eq 1 ]; then
+        # remove ${FUNC_NAME}.o of the schedule from workdir
+        rm ${FUNC_NAME}.o
+
+        ${CXX} -I${TIRAMISU_ROOT}/3rdParty/Halide/install/include -I${TIRAMISU_ROOT}/include -I${TIRAMISU_ROOT}/3rdParty/isl/include -Wl,--no-as-needed -ldl -g -fno-rtti -lpthread -fopenmp -std=c++17 -O0 -o ${FUNC_NAME}_unoptimized_generator.o -c ${FUNC_NAME}_unoptimized.cpp
+
+        ${CXX} -Wl,--no-as-needed -ldl -g -fno-rtti -lpthread -fopenmp -std=c++17 -O0 ${FUNC_NAME}_unoptimized_generator.o -o ./${FUNC_NAME}_unoptimized_generator -L${TIRAMISU_ROOT}/build -L${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64 -L${TIRAMISU_ROOT}/3rdParty/isl/build/lib -Wl,-rpath,${TIRAMISU_ROOT}/build:${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64:${TIRAMISU_ROOT}/3rdParty/isl/build/lib -ltiramisu -ltiramisu_auto_scheduler -lHalide -lisl
+
+        ./${FUNC_NAME}_unoptimized_generator
+
+        # create shared object from the generated object file
+        ${CXX} -shared -o ${FUNC_NAME}_unoptimized.o.so ${FUNC_NAME}.o
+
+        # compile the wrapper
+        ${CXX} -std=c++17 -fno-rtti -I${TIRAMISU_ROOT}/include -I${TIRAMISU_ROOT}/3rdParty/Halide/install/include -I${TIRAMISU_ROOT}/3rdParty/isl/include/ -I${TIRAMISU_ROOT}/benchmarks -L${TIRAMISU_ROOT}/build -L${TIRAMISU_ROOT}/3rdParty/Halide/install/lib64/ -L${TIRAMISU_ROOT}/3rdParty/isl/build/lib -o ${FUNC_NAME}_unoptimized_wrapper -ltiramisu -lHalide -ldl -lpthread -fopenmp -lm -Wl,-rpath,${TIRAMISU_ROOT}/build ./${FUNC_NAME}_wrapper.cpp ./${FUNC_NAME}_unoptimized.o.so -ltiramisu -lHalide -ldl -lpthread -fopenmp -lm -lisl
+    fi
+
+    # Running the compiled wrappers
+    printf "Running Schedule of ${FUNC_NAME} ...\n"
     ./${FUNC_NAME}_wrapper | tee ../results/${FUNC_NAME}.txt
+
+    if [ $EXECUTE_UNOPTIMIZED -eq 1 ]; then
+        printf "Running Unoptimized Schedule of ${FUNC_NAME} ...\n"
+        ./${FUNC_NAME}_unoptimized_wrapper | tee ../results/${FUNC_NAME}_unoptimized.txt
+    fi
 
     # remove the files from the WORKDIR_DIR
     rm -rf ${FUNC_NAME}_generator.cpp.o
@@ -94,6 +127,13 @@ for f in ./*.cpp; do
     rm -rf ${FUNC_NAME}.o
     rm -rf ${FUNC_NAME}.o.so
     rm -rf ${FUNC_NAME}_wrapper
+    if [ $EXECUTE_UNOPTIMIZED -eq 1 ]; then
+        rm -rf ${FUNC_NAME}_unoptimized_generator.cpp.o
+        rm -rf ${FUNC_NAME}_unoptimized_generator
+        rm -rf ${FUNC_NAME}.o
+        rm -rf ${FUNC_NAME}_unoptimized.o.so
+        rm -rf ${FUNC_NAME}_unoptimized_wrapper
+    fi
 
     printf "Done running ${FUNC_NAME}! Done: $(ls ../results | wc -l)/$(($(ls .. | wc -l) - 2))\n\n"
 
